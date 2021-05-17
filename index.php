ba63482e4da58ac80c0057149c08da7503fe30f7
Stuart Creed <stuart.a.creed@gmail.com>
2021-05-17 22:15:30 +0100

    https://www.codewars.com/kata/54da5a58ea159efa38000836/train/php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 
-echo 'Testing';
+// https://www.codewars.com/kata/54da5a58ea159efa38000836/train/php
+
+$func = fn(array $seq) => 'testing';
+var_dump($func([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]));
